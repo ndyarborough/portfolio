a5c306ba932a0a1c41230da0cf529b8f0ec7b26d
Noah Yarborough <ndyarborough1997@gmail.com>
2025-02-14 01:07:57 -0500

    Fixed author

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,7 @@
 import { TypeAnimation } from 'react-type-animation';
 import profilePic from '../../imgs/profilePic.jpg';
+import Pin from '../../imgs/pin.png';
+import Wave from '../../imgs/wave.png';
 import SocialButtons from '../SocialButtons/SocialButtons';
 import './AboutMe.css';
 
@@ -7,18 +9,27 @@ const AboutMe = ({ activeStyle }) => {
     return (
         <div className='aboutMe' id='about-me'>
             <div className='info'>
-                <h1>
-                    <TypeAnimation
-                        sequence={[
-                            1500,
-                            'Noah Yarborough', // Text to type out
-                        ]}
-                        wrapper="span"
-                        speed={50} // Speed of typing
-                        repeat={0} // Stops after one loop
-                        cursor={{show: false}}
-                    />
-                </h1>
+                <TypeAnimation
+                    className='name-header'
+                    sequence={[
+                        1500,
+                        'Noah Yarborough', // Text to type out
+                    ]}
+                    wrapper="span"
+                    speed={50} // Speed of typing
+                    repeat={0} // Stops after one loop
+                    cursor={{ show: false }}
+                />
+                <h2>Full-Stack Software Engineer</h2>
+                <div className='row'>
+                    <div className='row'>
+                        <img src={Pin} alt='Location Pin Icon' />
+                        <h2>Raleigh, NC</h2>
+                    </div>
+                    {/* <img src={Wave} alt='wave icon' className='wave'/> */}
+
+                </div>
+
                 <SocialButtons activeStyle={activeStyle} />
             </div>
 
